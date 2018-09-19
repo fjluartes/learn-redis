741968c5af44565345d971b4d49b205612b9ea4d
fjgl <fjluartes@gmail.com>
2018-09-19 14:29:32 +0800

    Edit js files for 2 space indentation

--- index.js
+++ index.js
@@ -3,20 +3,20 @@
 const Hapi = require('hapi');
 
 const server = new Hapi.server({
-    host: 'localhost',
-    port: 8080
+  host: 'localhost',
+  port: 8080
 });
 
 const init = async () => {
-    server.route(require('./routes'));
+  server.route(require('./routes'));
 
-    await server.start();
-    console.log(`Server running at ${server.info.uri}`);
+  await server.start();
+  console.log(`Server running at ${server.info.uri}`);
 };
 
 process.on('unhandledRejection', (err) => {
-    console.log(err);
-    process.exit(1);
+  console.log(err);
+  process.exit(1);
 });
 
 init();
--- routes.js
+++ routes.js
@@ -3,11 +3,11 @@
 const Main = require('./handlers/main');
 
 module.exports = [{
-    method: 'GET',
-    path: '/',
-    handler: Main.home
+  method: 'GET',
+  path: '/',
+  handler: Main.home
 }, {
-    method: 'GET',
-    path: '/pub-sub',
-    handler: Main.pubSub
+  method: 'GET',
+  path: '/pub-sub',
+  handler: Main.pubSub
 }];
